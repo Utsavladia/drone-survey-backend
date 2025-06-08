cf9c715f7cc6f64a3a6e3d11fec05820e2871f04
Utsavladia <utsavladia@gmail.com>
2025-06-09 02:01:15 +0530

    navigation

--- src/services/missionSimulationManager.js
+++ src/services/missionSimulationManager.js
@@ -181,7 +181,7 @@ function startSimulation(missionRunId, waypoints) {
             state.currentIndex++;
             state.currentPosition = { ...nextWaypoint };
         }
-    }, 2000);
+    }, 1000);
 
     return simulationState;
 }
